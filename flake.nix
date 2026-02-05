4d07e9d32b7006c6ad4e270b51156877b310f13d
Tim Dortmann <tdortman@students.uni-mainz.de>
2026-02-05 22:08:49 +0100

    replace figures from section 2.1 with tikz figures

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-new.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { nixpkgs, nixpkgs-new, ... }:
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
-        system = system;
-        config.allowUnfree = true;
-      };
-
-      pkgs-new = import nixpkgs-new {
         system = system;
         config.allowUnfree = true;
       };
@@ -22,7 +16,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           texliveFull
-          pkgs-new.tex-fmt
+          tex-fmt
         ];
       };
     };
